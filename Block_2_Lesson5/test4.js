3aacd27efbc335867148f7e6a736d164ab5c2502
Khuc Nhan <khucvii@gmail.com>
2023-11-28 10:44:23 +0700

    update

--- Block_2_Lesson5/test4.js
+++ Block_2_Lesson5/test4.js
@@ -1,4 +1,4 @@
 function djtme() {
     var selectedValue = document.getElementById("mySelect").value;
-    alert('Đéo đủ tiền, vui lòng nạp thêm!');
+    alert('Không đủ tiền, vui lòng nạp thêm!');
 }

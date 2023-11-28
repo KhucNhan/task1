5149cc0c8e1bb9aa90f07ab0938ab88b0b3ebfe0
Khuc Nhan <khucvii@gmail.com>
2023-11-28 08:41:21 +0700

    update

---
+++ Block_2_Lesson5/test3.js
@@ -0,0 +1,18 @@
+var firstName = "Khuc";
+var lastName = "Nhan";
+
+// console.log(firstName + ' Chi ' + lastName);
+
+// alert(firstName + ' Chi ' + lastName);
+
+// setInterval(function () {
+//     console.log(firstName + ' ' + lastName +  Math.random())
+// }, 3000
+// )
+
+// prompt('Thằng Khánh có bạn gái chưa ?');
+
+setInterval(function () {
+    console.log('Thằng nào đọc làm tró (trừ thằng code)' + ' ' + Math.random( ))
+}, 2000
+)
---
+++ Block_2_Lesson5/test4.js
@@ -0,0 +1,4 @@
+function djtme() {
+    var selectedValue = document.getElementById("mySelect").value;
+    alert('Đéo đủ tiền, vui lòng nạp thêm!');
+}
